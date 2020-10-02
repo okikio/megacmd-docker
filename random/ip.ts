6db0ba63911cd7a4b0d46256b7ec573d3044bdbd
Franz Knipp <franz@qnipp.com>
2020-10-02 15:54:54 +0000

    Improve used IP address range

--- random/ip.ts
+++ random/ip.ts
@@ -3,7 +3,7 @@ import { getInteger } from "./number.ts";
 const numberConfig = { min: 0, max: 256 };
 
 export function getIPv4() {
-  return `${getInteger({ min: 1, max: 256 })}.${getInteger(numberConfig)}.${
+  return `${getInteger({ min: 1, max: 224 })}.${getInteger(numberConfig)}.${
     getInteger(numberConfig)
   }.${getInteger(numberConfig)}`;
 }
